24fba5f586b911e8ac6ef6ea7360a5069965681e
raul2610 <rs.ruiza1@uniandes.edu.co>
2024-11-27 15:06:47 -0500

    arreglo

--- pruebas/pruebas/EncuestaPruebas.java
+++ pruebas/pruebas/EncuestaPruebas.java
@@ -1,4 +1,4 @@
-package totalPruebas;
+package pruebas;
 
 import static org.junit.jupiter.api.Assertions.*;
 
--- src/module-info.java
+++ src/module-info.java
@@ -7,4 +7,5 @@
 module Proyecto3 {
 	requires java.base;
 	requires org.json;
+	requires org.junit.jupiter.api;
 }
